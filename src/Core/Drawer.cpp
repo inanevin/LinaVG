0cf2eddac02caa808e02f41969fd85d01dcbf3c3
inanhh <inanevin@gmail.com>
2025-01-12 22:22:09 +0300

    sizecalc: fix for sdf vs non sdf

--- src/Core/Drawer.cpp
+++ src/Core/Drawer.cpp
@@ -2574,7 +2574,7 @@ namespace LinaVG
 
 		auto calcSizeChar = [&](TextCharacter& ch, GlyphEncoding c) {
 			float x = ch.m_advance.x * opts.textScale;
-			float y = (ch.m_bearing.y + ch.m_ascent) * opts.textScale;
+            float y = (ch.m_bearing.y + (opts.font->isSDF ? ch.m_ascent : 0.0f)) * opts.textScale;
 
 			if (opts.font->isSDF)
 			{
